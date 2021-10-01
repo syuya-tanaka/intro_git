e6a745c987f50e15868ee7477101866a73dba4fb
syuya-tanaka <horiemasafumi@horienoMacBook-Pro.local>
2021-10-01 17:01:43 +0900

    再帰関数の復習
    どういう風に書くのかを忘れていたので復習しました。

--- git_practice.py
+++ git_practice.py
@@ -3,3 +3,13 @@ print('say hello')
 print('git add')
 print('眠たいですね')
 print('いろいろありますね')
+
+
+def saiki(x):
+    while True:
+        if x < 0:
+            return 0
+        return saiki(x - 1) + x
+
+
+print(saiki(10))
