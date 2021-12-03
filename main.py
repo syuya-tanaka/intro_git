2734999c1e5063276b22ebe97dc02f6d121e083b
syuya-tanaka <horiemasafumi@horienoMacBook-Pro.local>
2021-12-03 11:44:20 +0900

    追加

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 # こちらmain.pyでございます
-print('Hello World2')
+print("Hello World2")
 
 # 変更を加える
+
+f = ["asdf", "asdfe", "sdf"]
+if "aaa" not in f:
+    print("eeeeeeeeeee")
+
+
+class Sample():
+    def sample(self):
+        self.x = 'eieie'
+
+
+class SSample(Sample):
+    def sample(self):
+        super().sample()
+        print(self.x)
+
+xx = SSample()
+xx.sample()
